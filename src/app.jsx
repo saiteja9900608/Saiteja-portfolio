eebc63259a1626f60a69c19425995cab569f124f
saiteja9900608 <saiteja3771@gmail.com>
2025-07-26 22:41:00 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Projects from './components/Projects';
+import Certifications from './components/Certifications';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="font-sans scroll-smooth">
+      <Navbar />
+      <main>
+        <Home />
+        <Projects />
+        <Certifications />
+        <Contact />
+      </main>
+    </div>
+  );
+}
+export default App;
 

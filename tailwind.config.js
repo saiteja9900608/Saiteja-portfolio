9f34f25642540ca177e1ef7d02de3afeccef04af
saiteja9900608 <saiteja3771@gmail.com>
2025-07-28 12:17:30 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#1E3A8A',
-        secondary: '#EEF2FF',
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };

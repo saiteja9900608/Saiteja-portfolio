0fa23c455967ef9214e03f9b2e6bddd04acf71ac
saiteja9900608 <saiteja3771@gmail.com>
2025-07-28 18:22:09 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import saitejapp from '../assets/saitejapp.jpg';
+import profile from '../assets/saitejapp.jpg';
 import { motion } from 'framer-motion';
 
 const Home = () => {
